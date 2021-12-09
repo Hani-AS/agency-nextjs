e75d0d30244e5c2bfe272c5ee2f876f15d96f6fc
Hani al Sabti <h.sabti87@gmail.com>
2021-12-09 18:56:17 +0100

    added some hardcoded data

---
+++ components/Services.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import styles from '../styles/Services.module.scss';
+
+const Services = () => {
+  return (
+    <div className={styles.container}>
+      <h1>What We Can Do?</h1>
+      <h2>Services we can help you with</h2>
+      <div className={styles.services}></div>
+    </div>
+  );
+};
+
+export default Services;
