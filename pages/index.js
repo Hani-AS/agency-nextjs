a05a709e33857a4bcb4548443f9bad9cca8a7348
Hani al Sabti <h.sabti87@gmail.com>
2021-12-09 19:04:32 +0100

    created getStaticProps to fetch data

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
 import Head from 'next/head';
 import Intro from '../components/Intro';
 import Services from '../components/Services';
+import { data } from '../data';
 import styles from '../styles/Home.module.scss';
 
-export default function Home() {
+export default function Home({ services }) {
+  console.log(services);
   return (
     <div>
       <Head>
@@ -11,7 +13,14 @@ export default function Home() {
         <meta name='description' content='Agency website' />
       </Head>
       <Intro />
-      <Services />
+      <Services services={services} />
     </div>
   );
 }
+
+export const getStaticProps = async () => {
+  const services = data;
+  return {
+    props: { services },
+  };
+};
