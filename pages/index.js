51570231ce2bb0f93bb80936ad5df46e34642ead
Hani al Sabti <h.sabti87@gmail.com>
2021-12-09 15:30:58 +0100

    Created base structure

---
+++ pages/index.js
@@ -0,0 +1,15 @@
+import Head from 'next/head';
+import Image from 'next/image';
+import styles from '../styles/Home.module.scss';
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Agency</title>
+        <meta name='description' content='Agency website' />
+      </Head>
+      This is homepage
+    </div>
+  );
+}
